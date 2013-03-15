056dac2b1f6abaa2f4a297b3b2aeac02c5bddcce
at VCC <lamdt@vccloud.vn>
2013-03-15 17:07:02 +0700

    new

--- share.php
+++ share.php
@@ -139,6 +139,20 @@
         
       </script>
       <img src="logo.jpg" style="display: none;">
+      <script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-39329617-1']);
+  _gaq.push(['_setDomainName', 'familug.org']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
     </div>
   </body>
 </html>
