b2c1867b6129120d21c325485318a1eb5bf3fda2
lamdt laptop <lamdt@vccloud.vn>
2013-03-16 08:16:52 +0700

    fix

--- player.php
+++ player.php
@@ -1,7 +1,7 @@
 <script src="build/mediaelement-and-player.min.js"></script>
 <link rel="stylesheet" href="build/mediaelementplayer.min.css" />
 <div id="player" style="position: fixed; bottom: 0; right: 0; background: #000; width: 100%">
-	<info style="color: #fff">Bài Hát: <?= $song['name'] ?> &bull; Ca Sĩ: <?= $song['singer'] ?> &bull; <a href="<?= $song['link'] ?>" target="_blank">Tải Về</a> &bull; <a href="s/<?= @$song['key'] ?>" >Link</a></info>
+	<info style="color: #fff">Bài Hát: <?= $song['name'] ?> &bull; Ca Sĩ: <?= $song['singer'] ?> &bull; <a href="<?= $song['link'] ?>" target="_blank">Tải Về</a> &bull; <a href="/s/<?= @$song['key'] ?>" >Link</a></info>
 	<audio width="100%" id="player2" src="<?= $song['link'] ?>" type="audio/mp3" controls="controls" autoplay="">		
 	</audio>
 </div>
